06caba7e8988a483878e50d1db0630a9c6f598aa
MohsenNasertabrizi <103938724+MohsenNasertabrizi@users.noreply.github.com>
2024-05-29 23:02:38 +0330

    Update Main.go

--- Web/Web_midrerm/Main.go
+++ Web/Web_midrerm/Main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"https://github.com/MohsenNasertabrizi/Spring2024/tree/main/Web/Web_midrerm/handler"
+	"https://github.com/MohsenNasertabrizi/Spring2024/tree/main/Web/Web_midrerm/model"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Initialize database
+	model.InitDB("shopping_cart.db")
+	defer model.CloseDB()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/basket/", handler.GetBaskets).Methods("GET")
+	r.HandleFunc("/basket/", handler.CreateBasket).Methods("POST")
+	r.HandleFunc("/basket/{id}", handler.UpdateBasket).Methods("PATCH")
+	r.HandleFunc("/basket/{id}", handler.GetBasket).Methods("GET")
+	r.HandleFunc("/basket/{id}", handler.DeleteBasket).Methods("DELETE")
+
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
